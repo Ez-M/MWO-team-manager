a6639748db9fc2d7aaf15b3d475b4e26cda5bb7d
Ezequiel <martinez.ezequiel42@gmail.com>
2022-07-18 21:36:04 -0700

    Created componentList as a master export list so I don't have to import a million lines in multiple pages

---
+++ src/components/componentList.js
@@ -0,0 +1,13 @@
+import React, {useState, useContext, Component} from "react";
+
+import MainHeader from "./mainHeader";
+import MainBody from "./mainBody";
+import MainFooter from "./mainFooter";
+
+
+
+
+
+
+
+export {MainHeader, MainBody, MainFooter}
